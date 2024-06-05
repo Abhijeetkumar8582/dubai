3069aae54fab64bfee07bcac1d234e5d044055c8
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-06-05 09:52:35 +0530

    Epiplex

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=97def47f-7ea8-429f-248a-08dc7b01a8b0&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        </script>
+      </body>
+    </Html>
+  );
+}
