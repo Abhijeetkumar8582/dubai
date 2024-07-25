07294bd3990c2ce0404ddd3e6bb88a2fa668e5db
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-07-25 20:11:00 +0530

    Oscar First commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=426ef051-7a77-4e9c-8257-08dc3d22de18&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=ec358588-6bd9-4159-e567-08dc65f02400&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
       </body>
     </Html>
